ae5db9a4ce8e398565a2cb44f0776ebc5b6a08c4
kane <yunzhong1912@163.com>
2018-01-29 15:07:30 +0800

    others

---
+++ others/computuer.c
@@ -0,0 +1,25 @@
+AMD Ryzen 5 1500X                   1299
+ROG STRIX B350-F GAMING             1099
+迪兰 RX580 8G X-Serial              2019
+金士顿HyperX Savage 8GB DDR4 2400   499
+希捷Barracuda 1TB 7200 n/s 64MB     329
+ANTEC VP600P                        399
+航嘉X-MAN                           599
+
+						TOTAL=6243
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
